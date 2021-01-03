99656d0a99d9f5a90768efa13c0aa0060f27462d
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-01-03 21:18:58 +0100

    Add action on button A to disable sounds

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 sound_level = 0
+sound_enabled = True
 bluetooth.start_button_service()
 
 def on_forever():
     global sound_level
+    global sound_enabled
     sound_level = input.sound_level()
     if sound_level > 100:
-        music.play_tone(262, music.beat(BeatFraction.WHOLE))
+        if sound_enabled:
+            music.play_tone(262, music.beat(BeatFraction.WHOLE))
         basic.show_icon(IconNames.GHOST)
     else:
         basic.clear_screen()
 basic.forever(on_forever)
+
+def on_button_pressed_a():
+    global sound_enabled
+    sound_enabled = not sound_enabled
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    sound_enabled = !(sound_enabled)
+})
 let sound_level = 0
+let sound_enabled = false
+sound_enabled = true
 bluetooth.startButtonService()
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     sound_level = input.soundLevel()
     if (sound_level > 100) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.showIcon(IconNames.Ghost)
+        if (sound_enabled) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+        }
+        basic.showIcon(IconNames.EigthNote)
     } else {
         basic.clearScreen()
     }
-    
 })
