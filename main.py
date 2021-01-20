6b7a1d0cb78c57a573bff040f433e35f94eb5319
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-01-20 16:02:24 +0100

    Add temperature on b button press

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+def on_button_pressed_a():
+    global sound_enabled
+    sound_enabled = not (sound_enabled)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global showing_temperature
+    showing_temperature = True
+    basic.show_number(input.temperature())
+    showing_temperature = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 sound_level = 0
-sound_enabled = True
+sound_enabled = False
+showing_temperature = False
+
 bluetooth.start_button_service()
 
 def on_forever():
-    global sound_level
-    global sound_enabled
+    global sound_level, showing_temperature
     sound_level = input.sound_level()
     if sound_level > 100:
         if sound_enabled:
             music.play_tone(262, music.beat(BeatFraction.WHOLE))
-        basic.show_icon(IconNames.GHOST)
+        basic.show_icon(IconNames.EIGTH_NOTE)
     else:
-        basic.clear_screen()
+        if not showing_temperature:
+            basic.clear_screen()
 basic.forever(on_forever)
-
-def on_button_pressed_a():
-    global sound_enabled
-    sound_enabled = not sound_enabled
-
-input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,18 +1,28 @@
-input.onButtonPressed(Button.A, function () {
-    sound_enabled = !(sound_enabled)
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    sound_enabled = !sound_enabled
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    showing_temperature = true
+    basic.showNumber(input.temperature())
+    showing_temperature = false
 })
 let sound_level = 0
 let sound_enabled = false
-sound_enabled = true
+let showing_temperature = false
 bluetooth.startButtonService()
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     sound_level = input.soundLevel()
     if (sound_level > 100) {
         if (sound_enabled) {
             music.playTone(262, music.beat(BeatFraction.Whole))
         }
+        
         basic.showIcon(IconNames.EigthNote)
-    } else {
+    } else if (!showing_temperature) {
         basic.clearScreen()
     }
+    
 })
